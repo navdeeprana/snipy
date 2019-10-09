d6673103bbf2edac6e726aa9afaa4cfcf0aa5426
navdeep rana <navdeepr@tifrh.res.in>
2019-10-09 14:55:14 +0530

    Set dl as well

--- diff.py
+++ diff.py
@@ -3,7 +3,7 @@ from scipy.ndimage import convolve
 
 # Few stencils for first and second order derivatives.
 
-def d1(u,axis,n_points=5,mode='wrap'):
+def d1(u,axis,dx = 1.e0, n_points=5,mode='wrap'):
     """
     First order derivative of a two dimensional array.
 
@@ -11,16 +11,18 @@ def d1(u,axis,n_points=5,mode='wrap'):
     n_points : Number of points to use in the stencil.
     mode     : scipy convolve mode. Use 'wrap' for periodic.
     """
+
+    dx_inv = dx
     if n_points == 5:
-        stencil = (1.e0/12.e0)*np.array([[-1,8,0,-8,1]])
+        stencil = (dx_inv/12.e0)*np.array([[-1,8,0,-8,1]])
     elif n_points == 3:
-        stencil = (1.e0/2.e0)*np.array([[-1,0,1]])
+        stencil = (dx_inv/2.e0)*np.array([[-1,0,1]])
     if axis == 'x':
         return convolve(u,stencil,mode=mode)
     if axis == 'y':
         return convolve(u,stencil.T,mode=mode)
 
-def d2(u,axis,n_points=5,mode='wrap'):
+def d2(u,axis,dx = 1.e0,n_points=5,mode='wrap'):
     """
     Second order derivative of a two dimensional array.
 
@@ -28,10 +30,11 @@ def d2(u,axis,n_points=5,mode='wrap'):
     n_points : Number of points to use in the stencil.
     mode     : scipy convolve mode. Use 'wrap' for periodic.
     """
+    dx_inv = dx
     if n_points == 5:
-        stencil = (1.e0/12.e0)*np.array([[-1,16,-30,16,-1]])
+        stencil = (dx_inv**2/12.e0)*np.array([[-1,16,-30,16,-1]])
     elif n_points == 3:
-        stencil = np.array([[1,-2,1]])
+        stencil = (dx_inv**2)*np.array([[1,-2,1]])
     if axis == 'x':
         return convolve(u,stencil,mode=mode)
     if axis == 'y':
