6c3ebd256d349323937346ec4616072cc9c0056e
navdeep rana <navdeepr@tifrh.res.in>
2020-01-25 14:32:20 +0530

    Gaussian fitting

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .analysis_helpers import line, fit_line
+from .analysis_helpers import line, fit_line, gaussian, fit_gaussian, fit_function
 from .calculus_helpers import d1, d2
 from .list_helpers import list_append_list, list_np, list_mean, list_std, list_log_spaced, list_pairwise_dict
 from .plot_helpers import no_ticks, default_colors, cmap_colors, ax_colorbar,fig_colorbar, ax_text, set_formatter
--- analysis_helpers.py
+++ analysis_helpers.py
@@ -4,6 +4,18 @@ from scipy.optimize import curve_fit
 def line(x,a,b):
     return a*x+b
 
+def gaussian(x,A,mu,sigma):
+    return (A/(np.sqrt(2*np.pi)*sigma))*np.exp(-0.5e0*((x-mu)/sigma)**2)
+
 def fit_line(x,y):
     popt,pcov = curve_fit(line,x,y)
     return popt, np.sqrt(np.diag(pcov))
+
+def fit_gaussian(x,y):
+    popt,pcov = curve_fit(gaussian,x,y)
+    return popt, np.sqrt(np.diag(pcov))
+
+def fit_function(x,y,function):
+    popt,pcov = curve_fit(function,x,y)
+    return popt, np.sqrt(np.diag(pcov))
+
