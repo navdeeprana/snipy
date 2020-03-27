798a5d01524d5fbc65bcffb177a35f091a6eb389
navdeep rana <navdeepr@tifrh.res.in>
2020-03-27 18:38:00 +0530

    statistics

--- __init__.py
+++ __init__.py
@@ -4,4 +4,4 @@ from .list_helpers import list_apply_fun, list_append_list, list_np, list_mean, 
 from .plot_helpers import no_ticks, default_colors, cmap_colors, ax_colorbar,fig_colorbar, ax_text, set_formatter, search_rcParams
 from .shell_helpers import get_folder
 from .yaml_helpers import read_yaml, write_yaml, collate_yaml_keywords
-
+from .statistics_helpers import ensemble_average
---
+++ statistics_helpers.py
@@ -0,0 +1,12 @@
+import sys
+import glob
+
+def ensemble_average(pattern,std=True,**kwargs):
+    files = sorted(glob.glob(pattern))
+    data  = np.asarray([np.loadtxt(f,**kwargs) for f in files])
+    if std:
+        return data.mean(axis=0), data.std(axis=0)
+    else:
+        return data.mean(axis=0)
+
+
