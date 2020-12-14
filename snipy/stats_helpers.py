d23124d9df8d5b28d9eeb8c4245aa15af1fc9864
navdeep rana <navdeepr@tifrh.res.in>
2020-12-14 09:03:55 +0530

    histogram functions

--- snipy/stats_helpers.py
+++ snipy/stats_helpers.py
@@ -13,5 +13,21 @@ def histogram(data,**kwargs):
     h,e = np.histogram(data,**kwargs)
     return 0.5e0*(e[1:]+e[:-1]), h
 
+def histogram_to_pdf(x,px):
+    psum = np.sum(px*(x[-1]-x[0]))/len(x)
+    return x, px/psum
+
+def get_mean_std(x,px):
+    L,N  = x[-1]-x[0], len(x)
+    DX   = L/N
+    mean = np.sum((x)*px*DX)
+    std  = np.sqrt(np.sum((x-mean)**2*px*DX))
+    return mean,std
+
+def normalize_histogram(x,px):
+    x,px=histogram_to_pdf(x,px)
+    m,s = get_mean_std(x,px)
+    return (x-m)/s, px*s
+
 def pdf_gaussian(x,mu,sigma):
     return (1/(sigma*np.sqrt(2*np.pi)))*np.exp(-0.5e0*((x-mu)/sigma)**2)
