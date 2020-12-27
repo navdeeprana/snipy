e47c086bf57c21b5a021068109f57252b160a16c
navdeep rana <navdeepr@tifrh.res.in>
2020-12-27 20:15:38 +0530

    Shortened

--- snipy/yaml_helpers.py
+++ snipy/yaml_helpers.py
@@ -1,15 +1,15 @@
 import ruamel_yaml as yaml
 
-def yaml_read(filename):
+def read(filename):
     with open(filename,'r') as f:
         data = yaml.load(f,Loader=yaml.Loader)
     return data
 
-def yaml_write(filename,data):
+def write(filename,data):
     with open(filename,'w') as f:
         yaml.dump(data,f,default_flow_style=None,indent=True)
 
-def yaml_collate_by_keywords(data,keys):
+def collate_by_keywords(data,keys):
     collated = []
     for k in keys:
         values = []
